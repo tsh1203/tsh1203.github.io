3ef0cb4bc8fd6bed3a51e385a45fef50bfadbfc2
tsh_qwq <David_tsh@163.com>
2026-07-09 15:48:46 +0800

    Update themeConfig.mjs

--- .vitepress/theme/assets/themeConfig.mjs
+++ .vitepress/theme/assets/themeConfig.mjs
@@ -318,7 +318,7 @@ export const themeConfig = {
   // 音乐播放器
   // https://github.com/imsyy/Meting-API
   music: {
-    enable: false,
+    enable: true,
     // url
     url: "https://api.injahow.cn/meting/",
     // id
