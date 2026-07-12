84221b3398fc5f8319c36e31011dee36e5c9ac7d
tsh_qwq <David_tsh@163.com>
2026-07-12 11:53:31 +0800

    Update index.js

--- .vitepress/theme/store/index.js
+++ .vitepress/theme/store/index.js
@@ -50,7 +50,7 @@ export const mainStore = defineStore("main", {
       // 上次滚动位置
       lastScrollY: 0,
       // 站点背景
-      backgroundType: "image",
+      backgroundType: "patterns",
       backgroundUrl: "https://tsh1203.github.io/images/background.png",
     };
   },
